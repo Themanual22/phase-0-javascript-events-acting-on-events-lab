9d3b61d5b0e3f68cd9e0d8e3f0817dd331c53f8e
Themanual22 <djamosry22@gmail.com>
2022-05-23 13:45:25 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ document.addEventListener("keydown", function (e) {
   if (e.key === "ArrowLeft") {
   moveDodgerLeft();
   }
+// });
+// function moveDodgerRight() {
+//   const leftNumbers = dodger.style.left.replace("px","");
+//   const left = parseInt(leftNumbers, 10);
+//   if (left < 360) {
+//     dodger.style.left = `${left + 1}px`;
+    
+//   }
+// }
+// document.addEventListener("keydown", function (e) {
+//   if (e.key === "ArrowRight") {
+//       moveDodgerRight() 
+//   }  
+// });
 });
 function moveDodgerRight() {
   const leftNumbers = dodger.style.left.replace("px","");
@@ -20,7 +34,8 @@ function moveDodgerRight() {
   }
 }
 document.addEventListener("keydown", function (e) {
-  if (e.key === "ArowRight") {
+  if (e.key === "ArrowRight") {
       moveDodgerRight() 
   }  
 });
+//more testing
